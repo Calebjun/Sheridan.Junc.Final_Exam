d4f1cf33a9141302d5e193c4d7796c2ef8cfdfd1
Caleb Jun <junc@sheridancollege.ca>
2026-04-14 11:09:17 -0400

    Initial commit - default project files

---
+++ src/test/java/sheridan/junc/final_exam/FinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.junc.final_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
